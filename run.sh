c3892545836a2b3f16d777775cbe9c92880f1196
thallock <dont@email.me>
2015-05-06 14:25:27 -0600

    other stuff i had forgot to commit

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export rootDir=/work/ballin-meme-share
 
 pushd $rootDir/ConvenienceShare/bin/
 
-java -cp .:../libs/org.json-20120521.jar:../libs/sqlite-jdbc-3.8.7.jar: org.cnv.shr.dmn.Main ../../runDir/settings1.props &
-java -cp .:../libs/org.json-20120521.jar:../libs/sqlite-jdbc-3.8.7.jar: org.cnv.shr.dmn.Main ../../runDir/settings2.props &
+java -cp .:../libs/h2-1.4.187.jar:../libs/org.json-20120521.jar:../libs/sqlite-jdbc-3.8.7.jar: org.cnv.shr.dmn.Main ../../runDir/settings1.props &
+java -cp .:../libs/h2-1.4.187.jar:../libs/org.json-20120521.jar:../libs/sqlite-jdbc-3.8.7.jar: org.cnv.shr.dmn.Main ../../runDir/settings2.props &
 
 popd
